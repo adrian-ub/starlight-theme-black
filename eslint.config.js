2121566ce76d196d2e81c656b08ccf2d4543ace3
Adrián UB <22903142+adrian-ub@users.noreply.github.com>
2026-07-13 22:37:41 -0500

    chore: update to Astro v6

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import antfu from '@antfu/eslint-config'
 export default antfu({
   formatters: true,
   astro: false,
+  pnpm: false,
   ignores: [
     'packages/starlight-theme-black/**/*.d.ts',
   ],
