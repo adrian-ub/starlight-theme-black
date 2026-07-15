efe9d035096b732da241a1fb3c47d52396447ec9
Adrián UB <22903142+adrian-ub@users.noreply.github.com>
2026-07-14 23:56:27 -0500

    feat: initial hero layout

--- packages/starlight-theme-black/schema.ts
+++ packages/starlight-theme-black/schema.ts
@@ -1,8 +1,17 @@
 import { z } from 'astro/zod'
 
 export const ExtendDocsSchema = z.object({
-  links: z.object({
-    doc: z.string().optional(),
-    api: z.string().optional(),
-  }).optional(),
+  hero: z
+    .object({
+      layout: z
+        .enum(['centered', 'media-top', 'media-left', 'media-right', 'banner'])
+        .default('media-left'),
+      announcement: z
+        .object({
+          text: z.string(),
+          link: z.string(),
+        })
+        .optional(),
+    })
+    .optional(),
 })
